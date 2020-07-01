013313a2c735ed0b56aaf3875153c41ba8f68592
lykis <lykismekk@outlook.com>
2020-07-01 15:53:49 +0800

    Initial commit

---
+++ src/main/java/com/selfStudy/quicksaleevent/QuickSaleEventApplication.java
@@ -0,0 +1,13 @@
+package com.selfStudy.quicksaleevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickSaleEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickSaleEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selfStudy/quicksaleevent/QuickSaleEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfStudy.quicksaleevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickSaleEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
