71ffa3896596bbe6ca666bfdc99564ef04f95ab6
lykis <lykismekk@outlook.com>
2020-07-04 11:26:53 +0800

    adding mobile num validation test

--- src/test/java/com/selfStudy/quicksaleevent/utils/EncoderTest/MD5Test.java
+++ src/test/java/com/selfStudy/quicksaleevent/utils/EncoderTest/MD5Test.java
@@ -8,10 +8,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class MD5Test {
+
     @Test
     public void encodeInput_thenReturnTrue() {
         String plainTest = "123456";
         String afterFirstEncoding = "d3b1294a61a07da9b49b6e22b2cbd7f9";
+        String encodingTwice = "b7797cce01b4b131b433b6acf4add449";
+
         assertEquals(afterFirstEncoding, MD5Util.inputPassToFormPass(plainTest));
+        assertEquals(encodingTwice, MD5Util.formPassToDBPass(afterFirstEncoding, MD5Util.getSalt()));
+        assertEquals(encodingTwice, MD5Util.inputPassToDBPass(plainTest, MD5Util.getSalt()));
     }
 }
